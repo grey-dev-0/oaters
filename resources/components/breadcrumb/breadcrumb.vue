c936f4082879afc5ef62a554b8efd6d1f36c26ec
Mohyaddin Saleh <mo7y.66@gmail.com>
2021-06-26 00:43:51 +0300

    Created new breadcrumb and content card vue components.

---
+++ resources/components/breadcrumb/breadcrumb.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="col-12">
+        <ul class="breadcrumb bg-white">
+            <slot></slot>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Breadcrumb"
+}
+</script>
---
+++ resources/components/breadcrumb/index.js
@@ -0,0 +1,7 @@
+import Breadcrumb from "./breadcrumb.vue";
+import BcItem from "./item.vue";
+
+export default {
+    Breadcrumb,
+    BcItem
+}

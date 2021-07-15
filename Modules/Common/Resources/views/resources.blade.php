ecc9ba13c67a0498f0f95fbdce578c24132d0994
Mohyaddin Saleh <mo7y..66@gmail.com>
2021-07-16 01:28:16 +0300

    Included blade UI kit.

--- Modules/Common/Resources/views/resources.blade.php
+++ Modules/Common/Resources/views/resources.blade.php
@@ -1,11 +1,13 @@
 <link rel="stylesheet" href="{{asset('resources/css/fontawesome.min.css')}}" type="text/css">
 <link rel="stylesheet" href="{{asset('resources/css/bootstrap.min.css')}}" type="text/css">
 <link rel="stylesheet" href="{{asset('resources/css/colors.min.css')}}" type="text/css">
+@bukStyles(true)
 <script type="text/javascript" src="{{asset('resources/js/jquery.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('resources/js/bootstrap.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('resources/js/bootbox.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('resources/js/vue.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('resources/js/vue-loader.min.js')}}"></script>
+@bukScripts(true)
 <script type="text/javascript">
     $.ajaxSettings.headers = {'X-CSRF-TOKEN': '{{csrf_token()}}'};
 </script>

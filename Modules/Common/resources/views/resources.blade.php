d0f6d2e292529104d432c8e7a679ef5d4e639662
Mohyaddin Saleh <mo7y.66@gmail.com>
2024-04-14 11:44:15 +0300

    Added required fontawesome static assets.

--- Modules/Common/resources/views/resources.blade.php
+++ Modules/Common/resources/views/resources.blade.php
@@ -1,4 +1,5 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<link rel="stylesheet" href="{{asset('css/fontawesome.min.css')}}" type="text/css">
 @vite(['resources/scss/bootstrap.scss'])
 <script type="text/javascript">
     var locale = {};

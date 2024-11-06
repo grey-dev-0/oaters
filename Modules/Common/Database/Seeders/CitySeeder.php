f0e9012792c692fecdfd360a808b05acd6891e91
Mohyaddin Alaoddin <mo7y.66@gmail.com>
2024-11-06 20:48:58 +0300

    Improved Cities seeding by using SQL instead of json files.

--- Modules/Common/Database/Seeders/CitySeeder.php
+++ Modules/Common/Database/Seeders/CitySeeder.php
@@ -10,11 +10,17 @@ class CitySeeder extends Seeder{
      * Run the database seeds.
      */
     public function run(): void{
-        foreach(['asia', 'africa', 'europe', 'north-america', 'central-america', 'south-america'] as $continent){
-            if(!file_exists($dataFile = base_path("storage/app/$continent-cities.json")))
-                continue;
-            $cities = json_decode(file_get_contents($dataFile), true);
-            City::factory()->sequence(...$cities)->count(count($cities))->create();
-        }
+        if(file_exists($file = base_path('storage/app/cities.sql.gz'))){
+            $queries = explode('insert into ', implode(gzfile($file)));
+            foreach($queries as $query)
+                if(!empty(trim($query)))
+                    \DB::unprepared("insert into $query");
+        } else
+            foreach(['asia', 'africa', 'europe', 'north-america', 'central-america', 'south-america'] as $continent){
+                if(!file_exists($dataFile = base_path("storage/app/$continent-cities.json")))
+                    continue;
+                $cities = json_decode(file_get_contents($dataFile), true);
+                City::factory()->sequence(...$cities)->count(count($cities))->create();
+            }
     }
 }
